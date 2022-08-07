d70308793086b1900cd08a38f62bf69a3f79f3b1
Anton Balter <antbalter@gmail.com>
2022-08-07 21:54:49 +0300

    HW15-solution

--- src/telran/util/ArrayList.java
+++ src/telran/util/ArrayList.java
@@ -5,40 +5,46 @@ import java.util.Iterator;
 import java.util.function.Predicate;
 
 public class ArrayList<T> implements List<T> {
-private static final int DEFAULT_CAPACITY = 16;
-private T[] array;
-private int size;
-@SuppressWarnings("unchecked")
-public ArrayList(int capacity) {
-	array = (T[]) new Object[capacity];
-}
-public ArrayList() {
-	this(DEFAULT_CAPACITY);
-}
-private class ArrayListIterator implements Iterator<T> {
-int currentInd = 0;
-boolean flNext = false;
-	@Override
-	public boolean hasNext() {
-		
-		return currentInd < size;
+	private static final int DEFAULT_CAPACITY = 16;
+	private T[] array;
+	private int size;
+
+	@SuppressWarnings("unchecked")
+	public ArrayList(int capacity) {
+		array = (T[]) new Object[capacity];
 	}
 
-	@Override
-	public T next() {
-		flNext = true;
-		return  array[currentInd++];
+	public ArrayList() {
+		this(DEFAULT_CAPACITY);
 	}
-	@Override 
-	public void remove() {
-		if(!flNext) {
-			throw new IllegalStateException();
+
+	private class ArrayListIterator implements Iterator<T> {
+		int currentInd = 0;
+		boolean flNext = false;
+
+		@Override
+		public boolean hasNext() {
+
+			return currentInd < size;
 		}
-		ArrayList.this.remove(--currentInd);
-		flNext = false;
+
+		@Override
+		public T next() {
+			flNext = true;
+			return array[currentInd++];
+		}
+
+		@Override
+		public void remove() {
+			if (!flNext) {
+				throw new IllegalStateException();
+			}
+			ArrayList.this.remove(--currentInd);
+			flNext = false;
+		}
+
 	}
-	
-}
+
 	@Override
 	public boolean add(T obj) {
 		if (array.length == size) {
@@ -50,47 +56,43 @@ boolean flNext = false;
 
 	@Override
 	public boolean remove(Object pattern) {
-		//array reallocation isn't done
-		//that is new array won't be created - essence of remove
-		//to use System.arraycopy
+		// array reallocation isn't done
+		// that is new array won't be created - essence of remove
+		// to use System.arraycopy
 		// size--
 		boolean res = false;
 		int index = indexOf(pattern);
 		if (index >= 0) {
-			res =true;
+			res = true;
 			removeByIndex(index);
 		}
-		
+
 		return res;
 	}
-	
+
 	private void removeByIndex(int index) {
 		size--;
-		System.arraycopy(array, index+1, array, index, size - index);
-		//array[size] == array[size - 1] => Memory leak
-		array[size] = null; //solution for preventing memory leak;
+		System.arraycopy(array, index + 1, array, index, size - index);
+		// array[size] == array[size - 1] => Memory leak
+		array[size] = null; // solution for preventing memory leak;
 	}
-
-	
-
-	
 
 	@Override
 	public int size() {
-		
+
 		return size;
 	}
 
 	@Override
 	public Iterator<T> iterator() {
-		
+
 		return new ArrayListIterator();
 	}
 
 	@Override
 	public boolean add(int index, T obj) {
-		//if size == array.length you should do reallocation see the method add
-				//if size < array.length new array won't be created - essence of the algorithm
+		// if size == array.length you should do reallocation see the method add
+		// if size < array.length new array won't be created - essence of the algorithm
 		boolean res = false;
 		if (index >= 0 && index <= size) {
 			res = true;
@@ -110,19 +112,20 @@ boolean flNext = false;
 		if (checkExistingIndex(index)) {
 			res = array[index];
 			removeByIndex(index);
-			
+
 		}
 		return res;
 	}
 
 	private boolean checkExistingIndex(int index) {
-		
+
 		return index >= 0 && index < size;
 	}
+
 	@Override
 	public int indexOf(Object pattern) {
 		int res = -1;
-		for(int i = 0; i < size; i++) {
+		for (int i = 0; i < size; i++) {
 			if (array[i].equals(pattern)) {
 				res = i;
 				break;
@@ -140,31 +143,41 @@ boolean flNext = false;
 				break;
 			}
 		}
-		
+
 		return res;
 	}
+
 	@Override
 	public T get(int index) {
-		
+
 		return checkExistingIndex(index) ? array[index] : null;
 	}
+
 	@Override
-	public boolean removeIf (Predicate<T> predicate) {		
-		int sizeOld = size();		
+	public boolean removeIf(Predicate<T> predicate) {
+		int sizeOld = size;		
+		return sizeOld > filterOutByPredicate(predicate, sizeOld);
+	}
+
+	private void removeGarbage(int sizeOld) {
+		for (int i = size; i < sizeOld; i++) {
+			array[i] = null; // hello garbage collector >-_[@-@]_-<
+		}
+	}
+
+	private int filterOutByPredicate(Predicate<T> predicate, int sizeOld) {
 		int count = 0;
-		for(int i = 0; i < sizeOld; i++) {
+		for (int i = 0; i < sizeOld; i++) {
 			if (predicate.test(array[i])) {
-				array[i] = array[i+1];
+				array[i] = array[i + 1];
 				count++;
-			} else if(i-1 >=0 &&  array[i].equals(array[i-1])) {
-				array[i--] = array[i+1];				
+			} else if (i - 1 >= 0 && array[i].equals(array[i - 1])) {
+				array[i--] = array[i + 1];
 			}
 		}
 		size = size - count;
-		for(int i = size; i < sizeOld; i++) {
-			array[i]
-		}
-		return sizeOld > size();
+		removeGarbage(sizeOld);
+		return size;
 	}
 
 }
